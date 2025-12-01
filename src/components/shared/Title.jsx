347a642ebdf6a998d61bf52b45b074bb43adae59
unknown <sumityadav2428@gmail.com>
2025-12-01 19:22:09 +0530

    frontend done

--- src/components/shared/Title.jsx
+++ src/components/shared/Title.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 
 const Title = ({
-  title = "Chat App",
-  description = "this is the Chat App called Chattu",
+  title = "TalkBridge",
+  description = "this is the Chat App called TalkBridge",
 }) => {
   return (
     <Helmet>
